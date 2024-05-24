b90d21064fd7330c62f817433accdca584078a47
Fernando Melo <fgmelofilho@icloud.com>
2024-05-24 12:09:37 -0400

    first commit, tabs and header created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  Heading,
+  Spacer,
+} from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Flex minWidth="max-content" alignItems="center" gap="2">
+        <Box p="2">
+          <Heading size="md">Criptoloucos</Heading>
+        </Box>
+        <Spacer />
+        <ButtonGroup gap="2">
+          <Button colorScheme="teal">Sign Up</Button>
+          <Button colorScheme="teal">Log in</Button>
+        </ButtonGroup>
+      </Flex>
+      <div className="body">
+        <Tabs variant="enclosed" colorScheme="green">
+          <TabList>
+            <Tab>Home</Tab>
+            <Tab>Add Investments</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </div>
     </div>
   );
 }
